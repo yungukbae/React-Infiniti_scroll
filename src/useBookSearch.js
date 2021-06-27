d824349cb4dd223d375282fb8ba667d98ed6ae28
yungukbae <yungukbae@gmail.com>
2021-06-28 01:09:16 +0900

    infiniti scroll

---
+++ src/useBookSearch.js
@@ -0,0 +1,17 @@
+import React,{useEffect, useState} from 'react';
+
+const useBookSearch = (query,pageNumber) => {
+
+    
+
+    useEffect(() => {
+
+        
+    },query,pageNumber)
+
+
+
+    return null
+}
+
+export default useBookSearch
